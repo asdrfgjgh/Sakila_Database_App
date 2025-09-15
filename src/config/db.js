36cef6558e9399426dc4f657bddfeadec60d383d
Ryan <ryantsoi12345@gmail.com>
2025-09-15 12:14:23 +0200

    terug knop bij favorites

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Wachtwoord123',
-  database: process.env.DB_DATABASE || 'sakila',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT || 3306,
   multipleStatements: true,
   waitForConnections: true,
